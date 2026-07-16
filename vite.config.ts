6a46a22428a5a8781730571bdddc1e3a2b059f60
Raknage <68710615+Raknage@users.noreply.github.com>
2026-07-16 19:51:55 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {
+    arrowParens: "always",
+    bracketSameLine: true,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    printWidth: 100,
+    sortImports: true,
+    sortTailwindcss: true,
+    tabWidth: 2,
+  },
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+    plugins: ["eslint", "typescript", "unicorn", "oxc", "react"],
+  },
+});
